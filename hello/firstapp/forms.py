4d5de3f1a2c5583a4eb8d7231e4b6bdebf69396b
Sergei Bykov <serega1988bykov@gmail.com>
2024-06-06 06:49:41 +0500

    using widget

--- hello/firstapp/forms.py
+++ hello/firstapp/forms.py
@@ -2,12 +2,6 @@ from django import forms
 
 
 class UserForm(forms.Form):
-    combo_text = forms.ComboField(label='Введите данные',
-                                  fields=[
-                                      forms.CharField(max_length=20),
-                                      forms.EmailField()])
-    combo_text2 = forms.MultiValueField(label='Комплексное поле',
-                                        fields=(
-                                            forms.CharField(max_length=20),
-                                            forms.EmailField()))
-    data_time = forms.SplitDateTimeField(label='Введите дату и время')
+    name = forms.CharField(label='Имя')
+    age = forms.IntegerField(label='Возраст')
+    comment = forms.CharField(label='Комментарий', widget=forms.Textarea)
