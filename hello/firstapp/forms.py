4675e061a24742d827dfadf81a0c8d0ed94c9573
Sergei Bykov <serega1988bykov@gmail.com>
2024-06-09 18:09:50 +0500

    init style of forms

--- hello/firstapp/forms.py
+++ hello/firstapp/forms.py
@@ -2,5 +2,9 @@ from django import forms
 
 
 class UserForm(forms.Form):
-    name = forms.CharField(label='Имя Клиента')
-    age = forms.IntegerField(label='Возраст клиента')
+    name = forms.CharField(label='Имя Клиента',
+                           widget=forms.TextInput(attrs={'class': 'myfield'}))
+    age = forms.IntegerField(label='Возраст клиента',
+                             widget=forms.TextInput(attrs={'class': 'myfield'}))
+    # required_css_class = 'field'
+    # error_css_class = 'error'
