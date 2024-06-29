b1b602380702554cf0ebc33c24d1b609c0e1958a
Sergei Bykov <serega1988bykov@gmail.com>
2024-06-29 11:47:33 +0500

    work with objets of moedls (read & write)

--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -1,13 +1,15 @@
 from django.http import HttpResponse, HttpResponsePermanentRedirect, HttpResponseRedirect, HttpResponseBadRequest, \
     HttpResponseForbidden
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.template.response import TemplateResponse
 from .forms import UserForm
+from .models import Person
 
 
 def index(request):
     my_text = 'Изучаем формы Django'
-    context = {'my_text': my_text}
+    people_kol = Person.object_person.count()
+    context = {'my_text': my_text, 'people_kol': people_kol}
     return render(request, 'firstapp/index.html', context)
 
 
@@ -20,13 +22,13 @@ def contact(request):
 
 
 def my_form(request):
-    if request.method=='POST':
-        userform = UserForm(request.POST)
-        if userform.is_valid():
-            name = request.POST.get('name')
-            age = request.POST.get('age')
-            output = '<h2>Пользователь</h2><h3>Имя - {0}, '\
-                'Возраст - {1} </h3>'.format(name, age)
-            return HttpResponse(output)
-    userform = UserForm()
-    return render(request, 'firstapp/my_form.html', {'form': userform})
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    my_text = 'Сведения о клиентах'
+    people = Person.object_person.all()
+    form = UserForm()
+    context = {'my_text': my_text, 'people': people, 'form': form}
+    return render(request, 'firstapp/my_form.html', context)
